6d537e322a5c0d24da418a6a1b14c688d54c4ab4
PratyushKumarPatro <85873207+PratyushKumarPatro@users.noreply.github.com>
2021-06-29 00:54:30 +0400

    Update Automotive recall management.sol

--- Automotive recall management.sol
+++ Automotive recall management.sol
@@ -570,7 +570,7 @@ function PlacePurchaseOrder(address receiver, string memory Partnumber, uint req
     }
     
 
-function ConfirmPurchaseOrder(bytes32 PurchaseOrderID,address Receiver2, uint quantitytoship, bool accepted) public onlyRequestor2{
+function ConfirmOrder(bytes32 PurchaseOrderID,address Receiver2, uint quantitytoship, bool accepted) public onlyRequestor2{
     require(Availableinventory>=quantitytoship,
     "Not enough stock available"
     );
